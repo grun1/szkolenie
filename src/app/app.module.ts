e6d9628ffc92ab32d9277f2018ec0e42c489b5aa
Michal Zielonka <michal.zielonka@exaco.pl>
2019-03-06 12:34:41 +0100

    tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageHomeComponent } from './page-home/page-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageHomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/page-home/page-home.component.spec.ts
@@ -0,0 +1,43 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PageHomeComponent } from './page-home.component';
+
+describe('PageHomeComponent', () => {
+  let component: PageHomeComponent;
+  let fixture: ComponentFixture<PageHomeComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PageHomeComponent ]
+    })
+    .compileComponents();
+  }));
+
+  afterEach (()=> {
+      fixture.debugElement.nativeElement.remove();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PageHomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should add simple numbers', () => {
+    expect(component.addNumbers(2,2)).toEqual(4);
+    expect(component.addNumbers(-2,2)).toEqual(0);
+    expect(component.addNumbers(-5,5)).toEqual(0);
+
+    expect(component.addNumbers('piotr',1)).toEqual(null);
+
+
+
+
+  });
+
+
+});
