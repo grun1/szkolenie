8322d676d5fc3074fa3111a2e6aa06e0de5bda6a
Michal Zielonka <michal.zielonka@exaco.pl>
2019-03-08 15:03:43 +0100

    testy

--- src/app/core/services/movies.service.spec.ts
+++ src/app/core/services/movies.service.spec.ts
@@ -1,18 +1,52 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, flush, fakeAsync } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-
 import { MoviesService } from './movies.service';
 
+
 describe('MoviesService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [
-      HttpClientTestingModule
-    ]
-  }));
-  
+  let service: MoviesService = null;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule ({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
+    service = TestBed.get(MoviesService);
+  });
+
 
   it('should be created', () => {
     const service: MoviesService = TestBed.get(MoviesService);
     expect(service).toBeTruthy();
   });
+
+  describe('fetchMovies', ()=>{
+    it('should exists', () => {
+      const service: MoviesService = TestBed.get(MoviesService);
+      expect(service.fetchMovies).toEqual(jasmine.any(Function));
+    });
+
+    it('should return long list of movies',fakeAsync(() => {
+      service.fetchMovies()
+      .then((movies)=>{
+        expect(movies.length).toEqual(8);
+      });
+    }));
+  });
+
+  describe('fetchPromoMovies', ()=>{
+    it('should be created', () => {
+      const service: MoviesService = TestBed.get(MoviesService);
+      expect(service.fetchPromoMovies).toEqual(jasmine.any(Function));
+    });
+  });
+
+  describe('fetchMovieById', ()=>{
+    it('should be created', () => {
+      const service: MoviesService = TestBed.get(MoviesService);
+      expect(service.fetchMovieById).toEqual(jasmine.any(Function));
+    });
+  });
+
 });
--- src/app/shared/pipes/user-display-name.pipe.spec.ts
+++ src/app/shared/pipes/user-display-name.pipe.spec.ts
@@ -1,8 +1,43 @@
 import { UserDisplayNamePipe } from './user-display-name.pipe';
+import { User } from '../interfaces/user';
 
 describe('UserDisplayNamePipe', () => {
+
+  let pipe: UserDisplayNamePipe = null
+
+  beforeEach(()=>{
+    pipe = new UserDisplayNamePipe();
+  });
+
   it('create an instance', () => {
     const pipe = new UserDisplayNamePipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should display name and surname', () => {
+   const user = <User>{
+     person:{
+       name: 'jan',
+       surname: 'nowak',
+     }
+   }
+   expect(pipe.transform(user)).toEqual('jan nowak');
+
+   const user1 = <User>{
+    person:{
+      name: '',
+      surname: '',
+    }
+  }
+  expect(pipe.transform(user1)).toEqual(' ');
+
+  const user2 = <User>{
+    person:{
+      name: ({}).toString(),
+      surname: null,
+    }
+  }
+  expect(pipe.transform(user2)).toEqual('[object Object] null');
+  });
+
 });
